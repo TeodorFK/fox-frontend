32afc0163f911c6a0fed9686a545feb2521dd807
TeodorFK <teodor.knutsen@gmail.com>
2026-04-14 12:02:32 +0200

    lagt til komentarer på koden og lagt til css.

--- controller/controller.js
+++ controller/controller.js
@@ -1,19 +1,24 @@
 const axios = require('axios');
 
+
+//Henter og viser rever på forsiden. Henter fra backend
 const index = async (req, res) => {
   const randomFox1 = await axios.get('http://10.12.5.12:4000/fox1');
   const randomFox2 = await axios.get('http://10.12.5.12:4000/fox2');
   try {
+    //Sender dataen til index.ejs
     res.render('index', { randomFox1, randomFox2 });
-    console.log(randomFox1);
   } catch (err) {
     console.log(err);
   }
 };
 
+// Sender score for en bestemt fox. Basert etter iden.
 const sendScore = async (req, res) => {
   try {
+    //Henter fox id
     const foxId = req.params.foxId;
+    //Sender post request til backend for å legge til ny score
     const result = await axios.post(`http://10.12.5.12:4000/score/${foxId}`);
     console.log(result);
     res.redirect('/');
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const controller = require('../controller/controller');
 
+//Hovedsiden
 router.get('/', controller.index);
 
+//Sender score til en spesefik fox ved bruk av foxid
 router.post('/score/:foxId', controller.sendScore);
 
 module.exports = router;
